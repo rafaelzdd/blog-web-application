8ba6951d968b8dfd25e5cd3d72e45e30acd7d94b
Rafael Pereira <rafael.zdd@gmail.com>
2024-08-10 19:50:47 -0300

    Builds getPosts function

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { getPosts } from "./posts.js";
 
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { title: "Home Page" });
+  const posts = getPosts();
+  res.render("index.ejs", { title: "Home", posts });
 });
 
 app.get("/new-post", (req, res) => {
